748c6e7a9419cd21e378e1878ae8d4364148c63f
vitoarruda8-wq <vitoarruda8@gmail.com>
2026-05-10 01:50:40 +0200

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import xml.etree.ElementTree as ET
 
 # CONFIGURATION
-WEBHOOK_URL = "https://discord.com/api/webhooks/1501291354641268801/AbpyLv1jv_Qru8fOQlqyEv98TbHhCdo-aLfJc8qM5YnzX9Mq_EucvUmNyA1ZD6CeJVh6"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1502819875804938260/qmyYsjsHJGMpvpSeWlTbu7D_uKSPi6_ZuBSERv5UJebs2nUqVgR35eSjmxdFJkQKIOmM"
 MARTIGNY_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Wappen_Martigny.svg/512px-Wappen_Martigny.svg.png"
 
 def get_level():
